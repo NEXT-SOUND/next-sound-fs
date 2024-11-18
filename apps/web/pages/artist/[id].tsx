6f6fb1cd46ef971fc923c39b83eb852f7f0f346d
Yusang Park <dfhilder@gmail.com>
2024-11-18 11:01:00 -0800

    translation for web

--- apps/web/pages/artist/[id].tsx
+++ apps/web/pages/artist/[id].tsx
@@ -21,7 +21,7 @@ function Home() {
   );
 }
 
-export async function getStaticProps({ locale }) {
+export async function getStaticProps({ locale }: { locale: string }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
